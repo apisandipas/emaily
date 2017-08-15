bbb38316adfbd50b90a390afaf85a418fc3d3a33
apis_and_ipas <bparonto@gmail.com>
2017-08-15 16:10:36 -0700

    adds payments for credits

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import Payments from './Payments'
 
 class Header extends Component {
   renderContent () {
-    switch(this.props.auth) {
+    switch (this.props.auth) {
       case null:
         return
       case false:
         return <li><a href='/auth/google'>Login With Google</a></li>
       default:
-       return <li><a href='/api/logout'>Log out</a></li>
+        return [
+          <li key={0}><Payments /></li>,
+          <li key={2} style={{ margin: '0 10px 0 20px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key={1}><a href='/api/logout'>Log out</a></li>
+        ]
     }
   }
   render () {
     return (
       <nav>
         <div className='nav-wrapper'>
-          <Link 
-            to={this.props.auth ? '/surveys' : '/'} 
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
             className='brand-logo'
           >
             Emaily
--- config/prod.js
+++ config/prod.js
@@ -1,15 +1,19 @@
 // prod.js
-// 
+
 const {
   GOOGLE_CLIENT_ID,
   GOOLE_CLIENT_SECRET,
   MONGO_URI,
-  COOKIE_KEY
+  COOKIE_KEY,
+  STRIPE_PUBLISHABLE_KEY,
+  STRIPE_SECRET_KEY
 } = process.env
 
 module.exports = {
   googleClientID: GOOGLE_CLIENT_ID,
   googleClientSecret: GOOLE_CLIENT_SECRET,
   mongoURI: MONGO_URI,
-  cookieKey: COOKIE_KEY
+  cookieKey: COOKIE_KEY,
+  stripePublishableKey: STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: STRIPE_SECRET_KEY
 }
